577a50ebd92fe278861c3dad65b83b88cca00961
Deeksha Sharma <deeksha232@gmail.com>
2017-11-04 18:18:39 +0530

    Initial commit

---
+++ boardgames/main/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+     return render(request,'main/home.html' ,{'message':'hi there!'})
+# Create your views here.
